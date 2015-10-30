12b9a8c070aa233fed6b3c4f7653b4bbf1443fb9
Padam Bhandari <Padam.Bhandari@datascan.com>
2015-10-30 10:32:30 -0500

    Constructor Demo added

---
+++ BaseConstructorDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseConstructorDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A classA = new A("Padam");
+            B classB = new B(2);
+            Console.Read();
+        }
+    }
+
+    // This a base class
+    public class A
+    {
+        public A(string a) // This is a base class
+        {
+            Console.WriteLine("From Constructor A");
+        }
+    }
+
+    public class B : A
+    {
+        public B(int value):base("Hello")
+        {
+            Console.WriteLine("From Derived Constructor B");
+        }
+    }
+}
