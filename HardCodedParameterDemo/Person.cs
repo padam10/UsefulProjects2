7763503d9e273d7066e36b9e5fbf018db5638295
Padam Bhandari <Padam.Bhandari@datascan.com>
2016-03-17 15:45:02 -0500

    hard coded parameter in a method and constructor

---
+++ HardCodedParameterDemo/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardCodedParameterDemo
+{
+    public class Person
+    {
+        public Person(int id, string name, int age = 5)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public int getTotal(int a, int b, int c = 10)
+        {
+            return a + b + c;
+        }
+    }
+}
---
+++ HardCodedParameterDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardCodedParameterDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person(2,"Padam",7);
+            Console.WriteLine("Id is {0}, Name is {1} and Age is {2}",p.Id,p.Name,p.Age);
+        
+            // if you provide a value in a parameter it will take that value otherwise it will take the hard coded value
+            //You can instantiate with just two parameters
+
+            Person p2 = new Person(2, "Padam");
+            Console.WriteLine("Id is {0}, Name is {1} and Age is {2}", p2.Id, p2.Name, p2.Age);
+
+            //if there is a hard coded value in the parameter you can call the method with or without the that particular
+            //parameter. If you provide it is going to be overided. 
+           int twoParam =  p2.getTotal(1, 2);
+            int threeParam = p2.getTotal(1, 2, 3);
+
+            Console.WriteLine("With two Param is: {0} and with three param is:{1}",twoParam,threeParam);
+
+            Console.Read();
+        }
+    }
+}
